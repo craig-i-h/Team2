66e220d1e5d303dbb95d62518a061f0c08bbee35
Douglas Couttie <douglas.couttie@atos.net>
2016-01-21 12:24:56 +0000

    [task 99574, update html to activate new routes, null]

--- svc/public/webapp/feature/home/home-controller.js
+++ svc/public/webapp/feature/home/home-controller.js
@@ -20,6 +20,10 @@
             return $state.is("home.item");
         };
 
+        vm.isAtAsset = function(){
+            return $state.includes("home.asset");
+        };
+
         vm.navigateToDashboard = function () {
             $state.go("home.dashboard");
         };
@@ -35,6 +39,17 @@
         vm.navigateToItem = function () {
             $state.go("home.item");
         };
+
+        vm.navigateToAsset = function (){
+            $state.go("home.asset");
+        };
+
+         vm.navigateToAddAsset = function (){
+                    $state.go("home.assetadd");
+                };
+         vm.navigateToAsset = function (){
+                    $state.go("home.assetammend");
+                        };
 
         vm.logout = function () {
             delete $window.sessionStorage.token;
